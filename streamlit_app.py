68a27d7c05f3e86c3c8c93ea5abe5afabba7da38
rainer-schuler <rainer.schuler.graz@gmail.com>
2026-05-20 19:59:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,8 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    st.text(smoothiefroot_response.json())
+    # st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
